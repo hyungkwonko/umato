9631bee36c82efcd11deb95fb6b2279b9b3df399
Hyung-Kwon Ko <hkko@hcil.snu.ac.kr>
2020-08-27 21:23:17 +0900

    refactoring using svelte each statement

--- visualization/src/App.svelte
+++ visualization/src/App.svelte
@@ -9,49 +9,18 @@
 
 <main>
 	{#if comparison}
-		<h1>{dnames[0].charAt(0).toUpperCase() + dnames[0].slice(1)} data</h1>
-		<table>
-			<tr>
-				<td><Scatterplot dname={dnames[0]} algoname={"pca"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[0]} algoname={"tsne"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[0]} algoname={"umap"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[0]} algoname={"topoae"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[0]} algoname={"umato"}></Scatterplot></td>
-			</tr>
-		</table>
-
-		<h1>{dnames[1].charAt(0).toUpperCase() + dnames[1].slice(1)} data</h1>
-		<table>
-			<tr>
-				<td><Scatterplot dname={dnames[1]} algoname={"pca"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[1]} algoname={"tsne"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[1]} algoname={"umap"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[1]} algoname={"topoae"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[1]} algoname={"umato"}></Scatterplot></td>
-			</tr>
-		</table>
-
-		<h1>{dnames[2].charAt(0).toUpperCase() + dnames[2].slice(1)} data</h1>
-		<table>
-			<tr>
-				<td><Scatterplot dname={dnames[2]} algoname={"pca"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[2]} algoname={"tsne"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[2]} algoname={"umap"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[2]} algoname={"topoae"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[2]} algoname={"umato"}></Scatterplot></td>
-			</tr>
-		</table>
-
-		<h1>{dnames[3].charAt(0).toUpperCase() + dnames[3].slice(1)} data</h1>
-		<table>
-			<tr>
-				<td><Scatterplot dname={dnames[3]} algoname={"pca"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[3]} algoname={"tsne"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[3]} algoname={"umap"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[3]} algoname={"topoae"}></Scatterplot></td>
-				<td><Scatterplot dname={dnames[3]} algoname={"umato"}></Scatterplot></td>
-			</tr>
-		</table>
+		{#each dnames as dname}
+			<h1>{dname.charAt(0).toUpperCase() + dname.slice(1)} data</h1>
+			<table>
+				<tr>
+					<td><Scatterplot dname={dname} algoname={"pca"}></Scatterplot></td>
+					<td><Scatterplot dname={dname} algoname={"tsne"}></Scatterplot></td>
+					<td><Scatterplot dname={dname} algoname={"umap"}></Scatterplot></td>
+					<td><Scatterplot dname={dname} algoname={"topoae"}></Scatterplot></td>
+					<td><Scatterplot dname={dname} algoname={"umato"}></Scatterplot></td>
+				</tr>
+			</table>
+		{/each}
 	{:else}
 		<h1>UMAP pipeline</h1>
 		<table>
@@ -75,12 +44,3 @@
 		</table>
 	{/if}
 </main>
-
-<!-- svg.append("line")
-  .attr("x1", 50)
-  .attr("y1", 50)
-  .attr("x2", 100)
-  .attr("y2", 100)
-  .attr("stroke", "#000")
-  .attr("stroke-width", 5)
-  .attr("marker-end", "url(#arrow)"); -->
