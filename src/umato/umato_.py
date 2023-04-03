ec9b6d4cc819d1506857a560474df1952f63bd5e
Hyeon Jeon <hj@hcil.snu.ac.kr>
2023-04-04 02:43:39 +0900

    updated umato to deal with the bug according to the number of hubs

--- src/umato/umato_.py
+++ src/umato/umato_.py
@@ -651,6 +651,8 @@ class UMATO(BaseEstimator):
         self.ll = ll
 
     def _validate_parameters(self):
+        if self.hub_num >= self._raw_data.shape[0]:
+            raise ValueError("hub_num must be less than the number of data points")
         if self.set_op_mix_ratio < 0.0 or self.set_op_mix_ratio > 1.0:
             raise ValueError("set_op_mix_ratio must be between 0.0 and 1.0")
         if self.gamma < 0.0:
