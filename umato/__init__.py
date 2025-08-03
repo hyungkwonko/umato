69ce770cd50b20ca42fceefceced80514f62907e
Sodaking <joshua2000@naver.com>
2025-08-04 01:29:18 +0900

    Consolidate license files and fix package version detection

--- umato/__init__.py
+++ umato/__init__.py
@@ -6,6 +6,6 @@ import numba
 from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = version("umap-learn")
+    __version__ = version("umato")
 except PackageNotFoundError:
     __version__ = "0.4-dev"
